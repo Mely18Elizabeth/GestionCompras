f65f38b35d7ae9ef2f29c9fdce6dffa78c0cc35f
Dayanna Chacha <jchachav@est.ups.edu.ec>
2025-04-24 10:17:14 -0500

    Comit Dayanna

--- src/main/java/ec/edu/ups/poo/clases/Proveedor.java
+++ src/main/java/ec/edu/ups/poo/clases/Proveedor.java
@@ -1,6 +1,32 @@
 package ec.edu.ups.poo.clases;
 
+import ec.edu.ups.poo.enums.Estado;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 public class Proveedor {
     private List<Producto> productos;
 
+    public Proveedor() {
+        productos = new ArrayList<>();
+    }
+    public Proveedor(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" +
+                "productos=" + productos +
+                '}';
+    }
 }
--- src/main/java/ec/edu/ups/poo/clases/Solicitud.java
+++ src/main/java/ec/edu/ups/poo/clases/Solicitud.java
@@ -1,11 +1,71 @@
 package ec.edu.ups.poo.clases;
+import ec.edu.ups.poo.enums.Estado;
 
 import java.util.GregorianCalendar;
 
 public class Solicitud {
     private String numero;
     private String nombreEmpleado;
-    private String cantidad;
+    private Double cantidad;
     private GregorianCalendar fechaInicio;
-    private EstadoSolicitud estado;
+    private Estado estado;
+
+    public Solicitud(String numero, String nombreEmpleado, Double cantidad, GregorianCalendar fechaInicio, Estado estado) {
+        this.numero = numero;
+        this.nombreEmpleado = nombreEmpleado;
+        this.cantidad = cantidad;
+        this.fechaInicio = fechaInicio;
+        this.estado = estado;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public Double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public GregorianCalendar getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(GregorianCalendar fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Solicitud{" +
+                "numero='" + numero + '\'' +
+                ", nombreEmpleado='" + nombreEmpleado + '\'' +
+                ", cantidad=" + cantidad +
+                ", fechaInicio=" + fechaInicio.getTime() +
+                ", estado=" + estado +
+                '}';
+    }
 }
